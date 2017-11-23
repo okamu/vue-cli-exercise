86fa40a83f5ebbd34f4f9f73668975bc6f1cd0a5
Hinako HIGA <hinako@HinakonoMacBook-Pro.local>
2017-11-24 01:24:22 +0900

    スタイルシート追加

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,58 +2,33 @@
   <div>
     <div>
       <small>Flat</small>
-      <md-button class="md-icon-button">
-        <md-icon>home</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-primary">
-        <md-icon>menu</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-accent">
-        <md-icon>thumb_up</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button" disabled>
-        <md-icon>add</md-icon>
-      </md-button>
+      <md-button>Default</md-button>
+      <md-button :md-ripple="false">Ripple Off</md-button>
+      <md-button class="md-primary">Primary</md-button>
+      <md-button class="md-accent">Accent</md-button>
+      <md-button disabled>Disabled</md-button>
     </div>
 
     <div>
       <small>Raised</small>
-      <md-button class="md-icon-button md-raised">
-        <md-icon>home</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-raised md-primary">
-        <md-icon>menu</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-raised md-accent">
-        <md-icon>thumb_up</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-raised" disabled>
-        <md-icon>add</md-icon>
-      </md-button>
+      <md-button class="md-raised">Default</md-button>
+      <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
+      <md-button class="md-raised md-primary">Primary</md-button>
+      <md-button class="md-raised md-accent">Accent</md-button>
+      <md-button class="md-raised" disabled>Disabled</md-button>
     </div>
 
     <div>
       <small>Dense</small>
-      <md-button class="md-icon-button md-dense md-primary">
-        <md-icon>person</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button md-dense md-raised md-primary">
-        <md-icon>cached</md-icon>
-      </md-button>
+      <md-button class="md-dense md-primary">Flat</md-button>
+      <md-button class="md-dense md-raised md-primary">Raised</md-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IconButtons'
+  name: 'RegularButtons'
 }
 </script>
 
