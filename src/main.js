50243a4c40e9e85cbf9218098331926519c7e78d
okamu <okamuka28@gmail.com>
2017-11-27 00:58:57 +0900

    Drawerを追加

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
+import PageHeader from './PageHeader'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/default-theme.css'
 
@@ -12,8 +12,9 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#header',
   router,
-  template: '<App/>',
-  components: { App }
+  components: {
+    'page-header': PageHeader
+  }
 })
